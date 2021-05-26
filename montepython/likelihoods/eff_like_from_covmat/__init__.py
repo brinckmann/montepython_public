ac5645ce1b0a3db61efc1032e756d7883033916b
Kim <Kim@Kims-MacBook-Air.local>
2021-05-26 12:13:39 +0200

    added eff_like_from_covmat folder

--- montepython/likelihoods/eff_like_from_covmat/__init__.py
+++ montepython/likelihoods/eff_like_from_covmat/__init__.py
@@ -4,27 +4,26 @@ import scipy.linalg as la
 from montepython.likelihood_class import Likelihood_prior
 from numpy.linalg import multi_dot
 
-
 class eff_like_from_covmat(Likelihood_prior):
     def __init__(self, path, data, command_line):
         # Call __init__ method of super class:
         super(eff_like_from_covmat, self).__init__(path, data, command_line)
-
+        self.covmat_inverse = la.inv(self.covmat)
+           
         self.need_cosmo_arguments(data,{'compute damping scale':'yes'})
 
     # Compute likelihood
     def loglkl(self, cosmo, data):
-
         covmat = self.covmat
         mu = self.mu
         mean_vec = np.array(mu)
-        covmat_inverse = la.inv(covmat)
-        mean_vec_class = [1.042195411207378,0.324186976388346] # 100*theta_s and 100*theta_d from CLASS
+        #mean_vec_class = np.array([eval(s) for s in self.get_var_strings])
+        mean_vec = [cosmo.theta_s_100(),cosmo.theta_d_100()]
+        mean_vec_class = np.array([eval(s) for s in mean_class])
+        #mean_vec_class = np.array([1.04219541,0.32418698]) 
         dif_vec = mean_vec - mean_vec_class
         dif_vec_T = dif_vec.T
-        #exponent = la.multi_dot([dif_vec.T,covmat_inverse,dif_vec])
-        #covmat_inverse = la.inv(self.covmat) # might delete covmat_inverse above
-        exponent = dif_vec_T.dot(covmat_inverse).dot(dif_vec)
+        exponent = dif_vec_T.dot(self.covmat_inverse).dot(dif_vec)
         loglikelihood = -1/2*exponent # exponent in the multivariate Gaussian
         return loglikelihood
 
